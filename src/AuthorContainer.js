2f5c28c7a49bf82338200fb0f9fe4578204728bf
normaescobar <norma@normaescobar.com>
2024-05-14 06:33:16 -0600

    feat:update base directory

--- src/AuthorContainer.js
+++ src/AuthorContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './AuthorContainer.css';
+import profilePicture from './assets/madame-president.png';
 
 const AuthorContainer = () => {
   return (
     <div className="author-container">
-      <img src="path/to/profile-picture.jpg" alt="Profile" className="profile-picture" />
-      <h2 className="profile-name">Norma Escobar</h2>
+      <img src={profilePicture} alt="Profile" className="profile-picture" />
+      <h2 className="profile-name">Madame President Research</h2>
       <p className="profile-tag">@madamepresident</p>
       <p className="profile-bio">
         My interpretations and aspirations of the world. Developer with a focus on Bitcoin and public practice.
